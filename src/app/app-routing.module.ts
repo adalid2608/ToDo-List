0a17cef579565b05c96454cdd0c0f4a9caad607a
adalid2608 <adalid.islas26@gmail.com>
2023-11-15 22:34:27 -0600

    Creación de los componentes, modulos y  rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  {
+    path: "app",
+    loadChildren: () => import ("./modules/todo/todo.module").then(m => m.TodoModule)
+  },
+  {
+    path: "**",
+    redirectTo: "app"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/todo/todo-routing.module.ts
@@ -0,0 +1,21 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { ListComponent } from './pages/list/list.component';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  {
+    path: "list",
+    component: ListComponent
+  },
+  {
+  path: "**",
+  redirectTo: "list"
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TodoRoutingModule { }
