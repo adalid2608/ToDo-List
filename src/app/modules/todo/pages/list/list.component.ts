9470464a2c84dad7ef78b8e485cd4f94e22af2b4
adalid2608 <adalid.islas26@gmail.com>
2023-11-15 23:38:06 -0600

    Creación del formulario para agregar una nueva tarea

--- src/app/modules/todo/pages/list/list.component.ts
+++ src/app/modules/todo/pages/list/list.component.ts
@@ -1,3 +1,5 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent {
+  fTask : FormGroup = this.fb.group({
+    name: [null, [Validators.required, Validators.minLength(5)]]
+  })
 
+  constructor (
+    private fb: FormBuilder 
+  ){}
+
+  saveTask() : void {
+    console.log(this.fTask.value);
+  }
 }
--- src/app/modules/todo/todo.module.ts
+++ src/app/modules/todo/todo.module.ts
@@ -1,9 +1,15 @@
-import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { CommonModule } from '@angular/common';
-
-import { TodoRoutingModule } from './todo-routing.module';
 import { ListComponent } from './pages/list/list.component';
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+import { NgModule } from '@angular/core';
+import { TodoRoutingModule } from './todo-routing.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +17,17 @@ import { ListComponent } from './pages/list/list.component';
   ],
   imports: [
     CommonModule,
-    TodoRoutingModule
+    TodoRoutingModule,
+
+    FormsModule,
+    ReactiveFormsModule,
+
+    MatIconModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class TodoModule { }
